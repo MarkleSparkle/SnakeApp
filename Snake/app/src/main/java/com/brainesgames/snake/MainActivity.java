5267f5de37b1382ca91bbbea96bb90601aad3ea9
MarkleSparkle <markfrezelljr@yahoo.ca>
2016-05-03 14:19:59 -0600

    -fixing failed push (no changes were actually made)

--- Snake/app/src/main/java/com/brainesgames/snake/MainActivity.java
+++ Snake/app/src/main/java/com/brainesgames/snake/MainActivity.java
@@ -1,8 +1,6 @@
 package com.brainesgames.snake;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -10,92 +8,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
 import android.view.Display;
 import android.view.View;
-import android.view.animation.AnimationUtils;
-import android.view.animation.RotateAnimation;
 import android.widget.TextView;
 
 import com.brainesgames.ascii.AsciiCanvas;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
-
 public class MainActivity extends AppCompatActivity {
 
-    String[] messages ={
-            "try flipping your device horizontally while playing",
-            "Plan in advance, drag your finger without releasing",
-            "Hehehe",
-            "Welcome lol",
-            "＼(*^￢^*)／",
-            "ヾ(｀д´*)ﾉ",
-            "≧(´▽｀)≦",
-            "（ノT＿T)ノ ＾┻━┻",
-            "｡：ﾟ(｡ﾉω＼｡)ﾟ･｡",
-            "♪(┌・。・)┌",
-            "The power within me!",
-            "do excuse us...",
-            "DID he die of hot dog poisoning?",
-            "Greedo shot first.",
-            "How the turn tables...",
-            "Excitement, HEH!",
-            "I would literally die for pop punk.",
-            "Tips are displayed on the main menu screen",
-            "Microwave offset foot quiver",
-            "Adventure, HEH!",
-            "Noodles and baguettes go well together.",
-            "Because ambient music.",
-            "Typically, tips will be shown on the main menu.",
-            "Guess and check is the best method",
-            "My gosh, my accelerometer's been stolen!",
-            "Why are firetrucks red?",
-            "How much wood a woodchuck chuck?",
-            "Because the molecules in the fire truck scatter red light more than blue.",
-            "Perforated.",
-            "Han shot first.",
-            "I trust you will do an okay job.",
-            "Join the HeckDivers!",
-            "I follow the teachings of Marx in my facial hair.",
-            "Jason Bourne, Jarl of somethin'",
-            "What IF you were here?",
-            "If this statement is true, the previous statement is false.",
-            "That's so contrapuntal.",
-            "Hey, Louis.",
-            "No True Scotsman would play this game.",
-            "There's a squid in my soup!",
-            "Vogel the Monkey!"
-    };
-    String[] colours ={
-            "#FFE95C",
-            "#00EDED",
-            "#FF1919",
-            "#D875FF",
-            "#00AA00",
-            "FFAB24",
-            "#8100EB"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int index = (int) (41*(Math.random())+5);
-        int colourIndex = (int) (7*(Math.random()));
-        int rotationValue = (int) (25*Math.random());
-        double negativeChance = (10*Math.random());
-        if(negativeChance < 5){
-            rotationValue = -1*(rotationValue);
-        }
-
-
-        TextView t = (TextView) findViewById(R.id.TextView01);
-        t.setText(messages[index]);
-        t.setTextColor(Color.parseColor(colours[colourIndex]));
-        t.setRotation(rotationValue);
-
-        TextView title = (TextView) findViewById(R.id.title);
+        TextView title=(TextView) findViewById(R.id.title);
         AsciiCanvas ac=AsciiCanvas.load(getResources().openRawResource(R.raw.titleart));
 
         //set title as big as possible
@@ -129,12 +54,10 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }*/
     /** Called when the user clicks the OPTIONS button */
@@ -146,5 +69,3 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this,OptionsActivity.class));
     }
 }
-
-
